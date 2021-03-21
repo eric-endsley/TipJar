a70d192d2d1a803e736bf2a48f3f88e8d50f8985
Eric Endsley <eric.endsley4@gmail.com>
2021-03-21 14:00:05 -0700

    refactor to use stylesheets, add form button to eventscreen

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Button } from 'react-native-paper';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function EventScreen({navigation}) {
-  <Text>Events will display here</Text>
-
+  return(
+    <View>
+      <Text>Events will display here</Text>
+      <Button style={styles.button} onPress={() => navigation.navigate('EventFormScreen')} icon="calendar-plus" mode="contained">
+            Add a New Event
+      </Button>
+    </View> 
+  )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'black',
+    marginRight: 15,
+    marginLeft: 15,
+  }
+})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Button } from 'react-native-paper';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 
   export default function HomeScreen({navigation}) {
-    const buttonStyle = {
-      backgroundColor: 'black',
-      marginRight: 15,
-      marginLeft: 15,
-    
-    }
-
     return(
       <>
       <View>
-        <Button style={buttonStyle} onPress={() => navigation.navigate('Login')} icon="account" mode="contained">
+        <Button style={styles.button} onPress={() => navigation.navigate('Login')} icon="account" mode="contained">
           Login/Register
         </Button>
-        <Button style={buttonStyle} onPress={() => console.log("View Events")} icon="microphone-variant" mode="contained">
+        <Button style={styles.button} onPress={() => navigation.navigate('Events')} icon="microphone-variant" mode="contained">
           View Events
         </Button>
       </View> 
       </>
     )
+      
+
   } 
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'black',
+    marginRight: 15,
+    marginLeft: 15,
+  }
+})
