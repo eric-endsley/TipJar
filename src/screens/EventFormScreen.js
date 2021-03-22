7e3d9806bc114c5d743a2221e90679e06ced1edc
Eric Endsley <eric.endsley4@gmail.com>
2021-03-21 16:31:12 -0800

    add local state to slider, move to customslider component

--- src/screens/EventFormScreen.js
+++ src/screens/EventFormScreen.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button } from 'react-native-paper';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 import Slider from '@react-native-community/slider';
 
 export default function EventFormScreen({navigation}) {
+  const [value, setValue] = useState(0);
   return(
     <View>
       <Text style={styles.header}>Add An Event</Text>
       <TextInput placeholder="Event Name"></TextInput>
       <TextInput placeholder="Performer Names"></TextInput>
-      <Text>Default tip amount</Text>
-      <Slider step={1} minimumValue={0} maximumValue={20} />
+      <Text>Default tip amount: ${value}</Text>
+      <Slider step={1} minimumValue={0} maximumValue={20} value={value} onValueChange={slideValue=> setValue(slideValue)} />
       <Button style={styles.button} onPress={() => console.log("submitted")} icon="calendar-plus" mode="contained">
             Add Event
       </Button>
