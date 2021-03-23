114383288f31fb71900bbcafaba0e825729f8759
Eric Endsley <eric.endsley4@gmail.com>
2021-03-22 21:56:53 -0700

    commit worklog

--- src/screens/EventFormScreen.js
+++ src/screens/EventFormScreen.js
@@ -3,15 +3,17 @@ import { Button } from 'react-native-paper';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 import CustomSlider from '../components/CustomSlider'
 import ImageForm from '../components/ImageForm'
+import firebase from '../components/Firebase'
+
+const dbh =firebase.firestore();
 
 export default function EventFormScreen({navigation}) {
-  const [value, setValue] = useState(0);
   return(
     <View>
       <Text style={styles.header}>Add An Event</Text>
-      <TextInput placeholder="Event Name"></TextInput>
-      <TextInput placeholder="Performer Names"></TextInput>
-      <ImageForm />
+      <TextInput name="name" placeholder="Event Name"></TextInput>
+      <TextInput name="peformers" placeholder="Performer Names"></TextInput>
+      <ImageForm name="image" />
       <CustomSlider />
       <Button style={styles.button} onPress={() => console.log("submitted")} icon="calendar-plus" mode="contained">
             Add Event
