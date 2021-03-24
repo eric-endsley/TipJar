092de63c6850e90e7b1532b2f788fc0b9fd57794
Eric Endsley <eric.endsley4@gmail.com>
2021-03-24 16:18:16 -0700

    modify getcollection function to structure data for use in flatlist

--- src/firebase/getCollection.js
+++ src/firebase/getCollection.js
@@ -4,7 +4,13 @@ const dbh = firebase.firestore();
 
 export default function getCollection(collectionName) {
   dbh.collection(collectionName).get().then((querySnapshot) => {
-      console.log(querySnapshot)
-      return querySnapshot;  
+      let dataArray = []
+      querySnapshot.forEach((doc) => {
+      const dataObj = doc.data();
+      dataObj["id"] = doc.id;
+      dataArray.push(dataObj)
+      })
+      console.log(dataArray)
+      return dataArray;
     })
   }
