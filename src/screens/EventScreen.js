e8b9de05365d252c59df53af6bafbf91c8d19d9b
Eric Endsley <eric.endsley4@gmail.com>
2021-03-24 17:23:18 -0700

    refactor eventscreen to use state and subscription listener

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Button } from 'react-native-paper';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { ActivityIndicator, View, Text, StyleSheet, FlatList } from 'react-native';
 import colors from '../config/colors'
-import getCollection from '../firebase/getCollection'
-
-const eventData = getCollection("events")
-console.log(eventData)
+import dataToFlatList from '../firebase/dataToFlatList'
+import firebase from '../firebase/Firebase'
 
 export default function EventScreen({navigation}) {
+  const [loading, setLoading] = useState(true);
+  const [eventData, setEventData] = useState([])
+
+  useEffect(() => {
+    const subscriber = firebase.firestore()
+      .collection('events')
+      .onSnapshot((querySnapshot => {
+        console.log(querySnapshot)
+        setEventData(dataToFlatList(querySnapshot));
+      }))
+    setLoading(false)
+    return () => subscriber();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
   return(
     <View style={styles.container}>
       <FlatList
         data={eventData}
-        keyExtractor={event => event.id}
-        renderItem={({event}) =>
-        <Text>{event.name} {event.performers}</Text>} 
-      />
+        keyExtractor={item => item.id}
+        renderItem={({item}) =>
+        <Text>{item.name} {item.performers}</Text>} />
       <Button style={styles.button} onPress={() => navigation.navigate('EventForm')} icon="calendar-plus" mode="contained">
-            Add a New Event
+          Add a New Event
       </Button>
     </View> 
   )
